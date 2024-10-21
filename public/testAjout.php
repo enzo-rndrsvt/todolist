5c5d774c8ff9b24a91c1f2da6d1f021c36f585d0
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-10-21 23:12:55 +0200

    test sql

--- public/testAjout.php
+++ public/testAjout.php
@@ -1,13 +1,15 @@
 <?php
 require 'scripts/bdd.php';
 $pdo = connectBDD();
-$nom = 'UtilisateurTest';
-$password = password_hash('mon_mot_de_passe', PASSWORD_BCRYPT);
+$nom = 'test';
+$password = password_hash('test', PASSWORD_BCRYPT);
 
 $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (:nom, :password)");
 $stmt->bindParam(':nom', $nom);
 $stmt->bindParam(':password', $password);
 $stmt->execute();
 
+header('Location: ../login.php');
+exit();
 
 ?>
