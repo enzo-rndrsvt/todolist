d4d7660c791e62f319c0aaa5bc452b88bb4998d4
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-10-22 15:19:00 +0200

    auth.css

--- public/register.php
+++ public/register.php
@@ -10,6 +10,7 @@ if (isset($_SESSION['user'])) {header('Location: index.php');exit();}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <link rel="stylesheet" href="./styles/auth.css"><body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
 <body>
@@ -24,7 +25,10 @@ if (isset($_SESSION['user'])) {header('Location: index.php');exit();}
             ?>
         <form action="scripts/userRegister.php" method="POST">
             <div><input type="text" id="nom" name="nom" placeholder="Nom d'utilisateur"required></div>
-            <div><input type="password" id="password" placeholder="Mot de passe" name="password" required></div>
+            <div class="password-container">
+                <input type="password" id="password" name="password" placeholder="Mot de passe" required>
+                <i class="fas fa-eye" id="togglePassword"></i>
+            </div>
             <div><input type="submit" value="Inscription"></div>
             <div>
                 <p>Déjà inscrit ? <a href="login.php">Connecte toi !</a></p>
