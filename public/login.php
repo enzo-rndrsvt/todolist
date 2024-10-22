25b0dbf5912164879f1fcf874542d5b73f89e616
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-10-22 15:16:05 +0200

    responsive

--- public/login.php
+++ public/login.php
@@ -10,6 +10,7 @@ if (isset($_SESSION['user'])) {header('Location: index.php'); exit();}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
     <link rel="stylesheet" href="./styles/auth.css"><body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
 <body>
@@ -23,12 +24,27 @@ if (isset($_SESSION['user'])) {header('Location: index.php'); exit();}
             ?>
         <form action="./scripts/userLogin.php" method="POST">
             <div><input type="text" id="nom" name="nom" placeholder="Nom d'utilisateur" required></div>
-            <div><input type="password" id="password" name="password" placeholder="Mot de passe" required></div>
+            <div class="password-container">
+                <input type="password" id="password" name="password" placeholder="Mot de passe" required>
+                <i class="fas fa-eye" id="togglePassword"></i>
+            </div>
             <div><input type="submit" value="Connexion"></div>
             <div>
                 <p>Pas encore inscrit ? <a href="register.php">Créer toi un compte</a></p>
             </div>
         </form>
     </div>
+    <script>
+        const togglePassword = document.querySelector('#togglePassword');
+        const password = document.querySelector('#password');
+
+        togglePassword.addEventListener('click', function (e) {
+            // toggle the type attribute
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            // toggle the eye slash icon
+            this.classList.toggle('fa-eye-slash');
+        });
+    </script>
 </body>
 </html>
